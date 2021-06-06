1ee64a2042e157cc2ea05a153b6112c788cb3cd9
tlpchk <max.telepchuk@gmail.com>
2021-06-06 21:21:58 +0200

    Base version of the tudy

--- src/grade_processing_utils.py
+++ src/grade_processing_utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-from .utils import basic_grading_dict, get_shaldon_value
+from .utils import basic_grading_dict, get_shaldon_value, basic_grading
 
 def coins_with_no_grade(df):
     df = df[df["title_state"].isna() & df["description_state"].isna()]
@@ -18,16 +18,17 @@ def unify_states(df):
     df.drop(["description_state", "title_state"], axis=1, inplace = True)
     return df
 
-def find_nearest(array, value):
+def find_nearest_idx(array, value):
     array = np.asarray(array)
     idx = (np.abs(array - value)).argmin()
-    return array[idx]
+    return idx
 
-def get_nearest_category_value(grade, categories=["1", "2", "3"]):
-    basic_grading_dict_inv = {v: k for k, v in basic_grading_dict.items()}
-    categories_in_sheldon = [basic_grading_dict[cat] for cat in categories]
-    nearest_sheldon_value = find_nearest(categories_in_sheldon, grade)
-    return basic_grading_dict_inv[nearest_sheldon_value]
+def get_nearest_category_value(sheldon, state, categories=["1", "1/2", "2", "2/3", "3"]):
+    if expert_grading(state) is not None:
+        return get_circulate_category(sheldon)
+    categories_in_sheldon = [basic_grading(cat) for cat in categories
+    nearest_category_idx = find_nearest_idx(categories_in_sheldon, grade)
+    return categories[nearest_category_idx]
     
 def get_circulate_category(sheldon):
     if sheldon >= 60:
@@ -39,8 +40,8 @@ def get_circulate_category(sheldon):
 
 def uncertain_categories(sheldon, state):
     categories = {
-        "1.5": ['1-/2', '1-/2+', '2+/1-'],
-        "2.5": ['2-', '2-/3', '2-/3+', '2/2-', '2/3', '3+/2', '3+/2-', '3/2', '~3+/2', '~3+/2-']
+        "1.5": ['1-/2', '1-/2+', '2+/1-'], # [56.5, 59.5] ,#
+        "2.5": ['2-', '2-/3', '2-/3+', '2/2-', '2/3', '3+/2', '3+/2-', '3/2', '~3+/2', '~3+/2-'] #[45, 40, 43.5, 48.5, 47]
     }
     for k,v in categories.items():
         if state in v:
@@ -53,8 +54,8 @@ def calculate_sheldon_and_categories(df, uncertain=False):
     df = unify_states(df)
     df["sheldon"] = df["state"].apply(get_shaldon_value)
     if uncertain:
-        df["category"] = df[["sheldon", "state"]].apply(lambda row: uncertain_categories(row[0], row[1]), axis=1 )
+        # df["category"] = df[["sheldon", "state"]].apply(lambda row: uncertain_categories(row[0], row[1]), axis=1 )
+        df["category"] = df[["sheldon", "state"]].apply(lambda row: get_nearest_category_value(row[0], row[1]), axis=1)
     else:
         df["category"] = df["sheldon"].apply(lambda grade: get_circulate_category(grade))
-        # df["category"] = df["sheldon"].apply(lambda grade: get_nearest_category_value(grade, categories))
     return df
